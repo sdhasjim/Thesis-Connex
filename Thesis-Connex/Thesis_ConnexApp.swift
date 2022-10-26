8a4ec28aa2948d58899a19e0c8efd51bb7337c91
Samuel Dennis <sdhasjim@gmail.com>
2022-10-26 20:35:47 +0700

    Initial Commit

---
+++ Thesis-Connex/Thesis_ConnexApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thesis_ConnexApp.swift
+//  Thesis-Connex
+//
+//  Created by Samuel Dennis on 26/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Thesis_ConnexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
