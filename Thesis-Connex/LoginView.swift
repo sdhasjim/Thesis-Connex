7f8347a3d301140d8f8419776d1d7f8fc18df318
Samuel Dennis <sdhasjim@gmail.com>
2022-12-04 21:25:44 +0700

    adding UI component for profile page, refactor firebase manager into util class

--- Thesis-Connex/LoginView.swift
+++ Thesis-Connex/LoginView.swift
@@ -10,26 +10,6 @@ import Firebase
 import FirebaseStorage
 import FirebaseFirestore
 
-class FirebaseManager: NSObject {
-    
-    let auth: Auth
-    let storage: Storage
-    let firestore: Firestore
-    
-    static let shared = FirebaseManager()
-    
-    override init() {
-        FirebaseApp.configure()
-        
-        self.auth = Auth.auth()
-        self.storage = Storage.storage()
-        self.firestore = Firestore.firestore()
-        
-        super.init()
-    }
-    
-}
-
 struct LoginView: View {
     @State var isLoginMode = true
     @State var createName = ""
--- Thesis-Connex/ProfileView.swift
+++ Thesis-Connex/ProfileView.swift
@@ -9,16 +9,69 @@ import SwiftUI
 
 struct ProfileView: View {
     @State var selectedTab = "profile"
+    @State var shouldShowImagePicker = false
+
     var body: some View {
-        ZStack{
-            Color("yellow_tone").ignoresSafeArea()
-            VStack{
-                Text("hai hola amigo")
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 32) {
+                    Spacer(minLength: 40)
+                    Button {
+                        shouldShowImagePicker.toggle()
+                    } label: {
+                        VStack {
+                            if let image = self.image {
+                                Image(uiImage: image)
+                                    .resizable()
+                                    .frame(width: 128, height: 128)
+                                    .scaledToFill()
+                                    .cornerRadius(64)
+                            } else {
+                                
+                                Image(systemName: "person.fill")
+                                    .font(.system(size: 64))
+                                    .padding()
+                                    .foregroundColor(Color(.label))
+                            }
+                        }
+                        .overlay(RoundedRectangle(cornerRadius: 64).stroke(Color.black, lineWidth: 3))
+                    }
+                    Text("Samuel Dennis")
+                    Text("sdhasjim@gmail.com")
+                }
+                
+                Button {
+//                    handleAction()
+                } label: {
+                    HStack {
+                        Spacer()
+                        Text("Log Out")
+                            .foregroundColor(.white)
+                            .padding(.vertical, 10)
+                            .font(.system(size: 14, weight: .semibold))
+                        Spacer()
+                    }
+                    .background(Color.red)
+                    .cornerRadius(10)
+                }
+                .padding()
+//                Text(self.loginStatusMessage)
+//                    .foregroundColor(.red)
             }
-//            CustomTabBar(selectedTab: $selectedTab)
-        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+//            .navigationTitle("Create Account")
+            .background(Color("yellow_tone")
+                .ignoresSafeArea())
+        }
+        
+        .fullScreenCover(isPresented: $shouldShowImagePicker, onDismiss: nil) {
+            ImagePicker(image: $image)
+        }
     }
+
+
+    @State var image: UIImage?
 }
+
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
